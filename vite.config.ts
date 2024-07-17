ec1a33dd770cbb3d6535adefa70c4270edb09944
hamawoo78 <nae.yk.jp@gmail.com>
2024-07-17 08:34:58 -0700

    change for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,22 +6,26 @@ export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.woff', '**/*.woff2', '**/*.ttf'],
 
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.prokerala.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-      '/api/token': {
-        target: 'https://api.prokerala.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/token/, '/token'),
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          // Add other headers as needed
-        },
-      },
-    },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://api.prokerala.com',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, ''),
+  //     },
+  //     '/api/token': {
+  //       target: 'https://api.prokerala.com',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api\/token/, '/token'),
+  //       headers: {
+  //         'Content-Type': 'application/x-www-form-urlencoded',
+  //         // Add other headers as needed
+  //       },
+  //     },
+  //   },
+  // },
+
+  build: {
+    outDir: 'dist', // Ensure the output directory is 'dist'
   },
 });
